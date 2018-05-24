3227123254fb71f3f2d51cd618982a19c0d86fc0
Tassio <tassiol.carmo@gmail.com>
2018-05-24 02:25:34 -0300

    mudei teste

---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,19 @@
+
+public class TestaPontoFlutuante 
+{
+	public static void main(String[] args)
+	{
+		double salario;
+		salario = 1250.515151;
+		
+		System.out.println("Meu salario é: " + salario);
+		
+		double divisao;
+		divisao = 3.14 / 2;
+		System.out.println(divisao);
+		
+		int outraDivisao = 5/2;
+		System.out.println(outraDivisao);
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,27 @@
+
+/**
+ * @author tassio
+ *
+ */
+public class TestaVariaveis {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		int idade = 37;
+		System.out.println("ola novo teste");
+		
+		System.out.println(idade);
+		
+		idade = 30 + 10;
+		
+		System.out.println(idade);
+		
+		idade = 7 * 2;
+		
+		System.out.println(idade);
+		
+		System.out.println("A idade é: "+idade);
+	}
+}
